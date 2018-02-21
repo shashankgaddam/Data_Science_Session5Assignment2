a5779cf83c520df9681f89293b25c8a71ac04639
shashankgaddam <34601798+shashankgaddam@users.noreply.github.com>
2018-02-21 20:55:34 +0530

    Add files via upload

---
+++ Untitled13 (2).py
@@ -0,0 +1,14 @@
+
+# coding: utf-8
+
+# In[9]:
+
+
+subjects = ["Americans","Indians"]
+verbs = ["play","watch"]
+objects = ["Baseball","Cricket"]
+for i in subjects:
+    for j in verbs:
+        for k in objects:
+            print(i+" "+j+" "+k+".")
+
